8d08979a3a75011837aa2f2b2160a1c834670613
Amanda Joan <amandajoanski@gmail.com>
2018-11-09 07:56:18 -0800

    added fun facts to data

--- data.js
+++ data.js
@@ -3,7 +3,7 @@
 
 var locationData = [{name: "Paris", AverageDailyCost: "91", AverageHotelCostNightly: "137", Fact: "It's believed that Paris only has one stop sign in the whole city."},
     {name: "Bangkok", AverageDailyCost: "64", AverageHotelCostNightly: "35", Fact: "Red Bull energy drink originated in Thailand."},
-    {name: "New York", AverageDailyCost: "120", AverageHotelCostNightly: "136", Fact: "Except for emergencies, hoking your horn is actually illegal in New York City..."},
+    {name: "New York", AverageDailyCost: "120", AverageHotelCostNightly: "136", Fact: "Except for emergencies, honking your horn is actually illegal in New York City..."},
     {name: "Bali", AverageDailyCost: "58", AverageHotelCostNightly: "34", Fact: "Bali is actually three islands."},
     {name: "Istanbul", AverageDailyCost: "35", AverageHotelCostNightly: "28", Fact: "Istanbul is the only city in the world that straddles two continents: Asia and Europe."},
     {name: "Lima", AverageDailyCost: "60", AverageHotelCostNightly: "38", Fact: "After Cairo in Egypt, Lima is considered the second largest desert city in the world. (Bring Suncreen)"}
